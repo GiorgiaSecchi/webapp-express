d80db0c7e6034d9267efe2036eb2635b3a7927ac
GiorgiaSecchi <secchigrg@gmail.com>
2025-01-10 16:05:12 +0100

    add moviesController

---
+++ controllers/moviesController.js
@@ -0,0 +1,9 @@
+//# INDEX
+
+function index(req, res) {
+  res.json({
+    message: "OK",
+  });
+}
+
+module.exports = { index };
--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "OK",
-  });
-});
+const moviesController = require("../controllers/moviesController.js");
+
+//# ROTTE MOVIES
+
+//* INDEX
+
+router.get("/", moviesController.index);
 
 module.exports = router;
