c87c13bbe3615af6892ab369eba8f00c45d84bea
GiorgiaSecchi <secchigrg@gmail.com>
2025-01-10 15:57:54 +0100

    add moviesRouter

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-//# CONFIG EXPRESS:
-
-//* INIT EXPRESS
+//# INIT EXPRESS
 const express = require("express");
 const app = express();
-
 const { APP_HOST, APP_PORT } = process.env;
 
-//* START LISTENING
+//# REGISTERING ROUTERS
+// import routers
+const moviesRouter = require("./routers/moviesRouter");
+// setting routers
+app.use("/movies", moviesRouter);
+
+//# START LISTENING
 app.listen(APP_PORT, () => {
   console.log(`Server listening at ${APP_HOST}:${APP_PORT}`);
 });
