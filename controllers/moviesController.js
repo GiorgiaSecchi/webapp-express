080cf58601427fa23f3a1c37e4384e23b3965cd8
GiorgiaSecchi <secchigrg@gmail.com>
2025-01-10 18:34:27 +0100

    mod show (add review movie)

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -15,8 +15,16 @@ function index(req, res) {
   connection.query(sql, (err, results) => {
     if (err) {
       console.log(err);
-      return res.status(500).json({ error: "Database query failed" });
+      return res.status(500).json({
+        status: "KO",
+        message: "Database query failed",
+      });
     }
+
+    // const movies = results.map(movies => ({
+    //   ...movies,
+    //   image: generateMovieImagePath(movies.image)
+    // }))
 
     res.json({
       status: "OK",
@@ -32,20 +40,57 @@ function index(req, res) {
 
 function show(req, res) {
   // recuperiamo l'id dall' URL
-  const id = parseInt(req.params.id);
+  const movieid = parseInt(req.params.id);
 
-  const sql = "SELECT * FROM movies WHERE id = ?";
-  connection.query(sql, [id], (err, results) => {
+  const sqlMovie = `
+  SELECT id, title, director, genre, release_year, abstract, image
+  FROM movies
+   WHERE id = ?
+  `;
+
+  connection.query(sqlMovie, [movieid], (err, results) => {
     if (err) {
       console.log(err);
-      return res.status(500).json({ error: "Database query failed" });
+      return res.status(500).json({
+        status: "KO",
+        message: "Database query failed",
+      });
     }
 
-    if (results.length === 0)
-      return res.status(404).json({ error: "Movies not found" });
+    const [movie] = results;
 
-    res.json(results[0]);
+    if (!movie) return res.status(404).json({ error: "Movie not found" });
+
+    // movie.image = generateMovieImagePath(movie.image)
+
+    const sqlReviews = `
+    SELECT id, name, vote, text, created_at
+    FROM reviews
+    WHERE movie_id = ?
+    `;
+
+    connection.query(sqlReviews, [movieid], (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          status: "KO",
+          message: "Database query failed",
+        });
+      }
+
+      movie.reviews = results;
+
+      res.json({
+        status: "OK",
+        movie,
+      });
+    });
   });
 }
+
+// const generateMovieImagePath = (imageName) => {
+//    const { APP_HOST, APP_PORT } = process.env;
+//   return `${APP_HOST}:${APP_PORT}/img/movies${imageName}`
+// }
 
 module.exports = { index, show };
